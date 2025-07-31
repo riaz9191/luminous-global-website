76fcbc86ffe96825f306f553df8aa4ed78a3b1f6
Riaz <riaz91a@gmail.com>
2025-07-31 13:33:13 +0600

    `Update className in Footer component`

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,9 @@ import { GeistMono } from 'geist/font/mono'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Luminous Global LLC',
+  description: 'Global FMCG & Ecommerce Distribution Simplified',
+  generator: 'Riaz Ahammed',
 }
 
 export default function RootLayout({
